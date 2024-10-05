69ef65e20b1cb4f5ed8f0e973175f4a376bf50cd
nullsploit01 <harshaldharmik099@gmail.com>
2024-10-05 07:58:43 +0530

    advanced day 1

---
+++ 02_advanced/assignment.py
@@ -0,0 +1,17 @@
+
+# GUESSING GAME
+# generate a random number (1-100)
+# Prompt an user to guess the correct number
+# define hints
+# difference less than 10 -> close
+# difference less than 5 -> really close
+# difference more than 11 -> little high
+# difference more than 25 -> little too high
+# difference more than 40 -> too high
+# If user guesses a higher number than correct number, (correct number = 75, user=85) you say close
+# You give them 5 tries before saying "Game Over"
+# Ask user for username
+# Create a game menu
+# Options -> View high Score, Leaderboards, Play game
+# It should be able to store high scores of multiple users in a file in format ({username: score})
+# Example: {"sagar": 10, "harshal": 8...}
---
+++ 02_advanced/modules.py
@@ -0,0 +1,13 @@
+
+
+import random
+correct_answer = random.randint(1,100)
+user_answer = int(input("Enter number guess: "))
+if abs(correct_answer-user_answer) <= 5:
+    print("really close. ")
+if abs(correct_answer-user_answer) <= 10:
+    print("close. ")
+if abs(correct_answer-user_answer) <= 15:
+    print("little far. ")
+if abs(correct_answer-user_answer) <= 20:
+    print("way far. ")
